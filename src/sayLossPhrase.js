39ece018d1694003cbfd75782c3d958145cb023b
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-03-20 22:44:15 +0500

    refactor sayLossPhrase: remove operator '+'

--- src/sayLossPhrase.js
+++ src/sayLossPhrase.js
@@ -1,8 +1,5 @@
 function sayLossPhrase(currect, yourAnswer, name) {
-  return (
-    `'${yourAnswer}' is wrong answer ;(. Correct answer was '${currect}'.` +
-    `\nLet's try again, ${name}!`
-  );
+  return `'${yourAnswer}' is wrong answer ;(. Correct answer was '${currect}'. \nLet's try again, ${name}!`;
 }
 
 export default sayLossPhrase;
