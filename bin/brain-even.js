9526ee65258e568d57a3a92b1a172fe4f98de58b
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-03-22 19:42:09 +0500

    chore: rename folder 'brain-even' to 'brain-games'

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import sayHelloByName from '../src/askNameSayHello.js';
-import gameProcess from '../src/brain-even/evenGames.js';
+import gameProcess from '../src/brain-games/evenGames.js';
 
 const name = sayHelloByName();
 gameProcess(name);
