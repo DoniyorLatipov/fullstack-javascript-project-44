4ddb3b2bdb2502b3f1ccfa2e0ece6d687856f041
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-03-23 15:10:39 +0500

    refactor evenGame: now use question lib in evenGame.js

--- src/brain-games/evenGame.js
+++ src/brain-games/evenGame.js
@@ -1,14 +1,14 @@
 import ckeckAnswer from '../answerCheck.js';
 import phrases from '../phrase.js';
+import gameQuestions from '../questions.js';
 
 function gameProcess(name) {
   let score = 0;
 
   while (score < 3) {
-    const questionNumber = _.random(1, 20);
-    phrases.askQuestion(questionNumber);
+    const { question, correctAnswer } = gameQuestions.evenGameQuestion();
+    phrases.askQuestion(question);
     const yourAnswer = phrases.getAnswer();
-    const correctAnswer = `${questionNumber % 2 === 0 ? 'yes' : 'no'}`;
 
     if (ckeckAnswer(correctAnswer, yourAnswer)) {
       phrases.correct();
