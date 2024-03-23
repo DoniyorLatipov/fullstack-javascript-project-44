0c92d9be6c4efb43d5bbfc302ce6353439d97708
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-03-23 20:50:02 +0500

    style: rename variable in function "calc" num => number

--- src/questions.js
+++ src/questions.js
@@ -15,14 +15,16 @@ const questions = {
     const num2 = _.random(1, 20);
     const currentOperator = operators[_.random(0, operators.length - 1)];
 
-    function calc(num1, num2, operator) {
+    function calc(number1, number2, operator) {
       switch (operator) {
         case '+':
-          return num1 + num2;
+          return number1 + number2;
         case '-':
-          return num1 - num2;
+          return number1 - number2;
         case '*':
-          return num1 * num2;
+          return number1 * number2;
+        default:
+          return null;
       }
     }
 
