6a02318c17a714891ac4e44733b443c084e19e23
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-03-23 18:53:31 +0500

    feat: add a new game "brain-calc" using existing modules
    creat a game module such as evenGame.js with main logic of the game
    but most often I use existing modules and special function from
    question.js to create a question task

---
+++ src/brain-games/calcGame.js
@@ -0,0 +1,27 @@
+import ckeckAnswer from '../answerCheck.js';
+import phrases from '../phrase.js';
+import gameQuestions from '../questions.js';
+
+function gameProcess(name) {
+  let score = 0;
+
+  while (score < 3) {
+    const { question, correctAnswer } = gameQuestions.calcGameQuestion();
+    phrases.askQuestion(question);
+    const yourAnswer = phrases.getAnswer();
+
+    if (ckeckAnswer(correctAnswer, yourAnswer)) {
+      phrases.correct();
+      score += 1;
+    } else {
+      phrases.youLoss(correctAnswer, yourAnswer, name);
+      break;
+    }
+  }
+
+  if (score === 3) {
+    phrases.youWin(name);
+  }
+}
+
+export default gameProcess;
