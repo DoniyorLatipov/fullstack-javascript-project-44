9cacd70a60f53fad35e693aca802d32ffc4474cf
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-03-23 11:16:56 +0500

    feat: creat new lib for phrases phrase.js
    the library has functionality for greetings, questions, answers, etc.

---
+++ src/phrase.js
@@ -0,0 +1,36 @@
+import { question } from 'readline-sync';
+
+const phrases = {
+  //*** introduction ***
+  welcome: function () {
+    console.log('Welcome to the Brain Games!');
+  },
+  getName: function () {
+    question('May I have your name? ');
+  },
+  sayHi: function (name) {
+    console.log(`Hello, ${name}!`);
+  },
+  //*** games instuction ***
+  evenGameInstruction: function () {
+    console.log('Answer "yes" if the number is even, otherwise answer "no".d');
+  },
+  // *** game mechanics ***
+  askQuestion: function (question) {
+    console.log(`Question: ${question}`);
+  },
+  getAnswer: function () {
+    question('Your answer: ');
+  },
+  // *** ending ***
+  sayLossPhrase: function (currect, yourAnswer, name) {
+    console.log(
+      `'${yourAnswer}' is wrong answer ;(. Correct answer was '${currect}'. \nLet's try again, ${name}!`
+    );
+  },
+  congratulations: function (name) {
+    console.log(`Congratulations, ${name}!`);
+  },
+};
+
+export default phrases;
