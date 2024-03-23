cdd65f7b36c48ef8111a1e6b3765f020405488c6
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-03-23 12:20:19 +0500

    refactor answerCheck: now just compares the answers

--- src/answerCheck.js
+++ src/answerCheck.js
@@ -1,13 +1,5 @@
-import sayLossPhrase from '../sayLossPhrase.js';
-
-function ckeckAnswer(correctAnswer, yourAnswer, score, name) {
-  if (yourAnswer === correctAnswer) {
-    console.log('Correct!');
-    score += 1;
-  } else {
-    console.log(sayLossPhrase(correctAnswer, yourAnswer, name));
-    process.exit(1);
-  }
+function ckeckAnswer(correctAnswer, yourAnswer) {
+  return correctAnswer === yourAnswer;
 }
 
 export default ckeckAnswer;
