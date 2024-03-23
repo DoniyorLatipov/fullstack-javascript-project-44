23c2908aba87ae569501b6824ab6c78942d49595
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-03-23 18:45:11 +0500

    refactor question: replace "eval()" with a more secure "switch-case"
    previously eval was used to convert mathematical expressions, but
    due to insecurity, I decided to change the code towards "switch-case"

--- src/questions.js
+++ src/questions.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
 const questions = {
-  evenGameQuestion: function () {
+  evenGameQuestion: function getQuestionsAboutEven() {
     const questionNumber = _.random(1, 20);
 
     return {
@@ -9,15 +9,26 @@ const questions = {
       correctAnswer: `${questionNumber % 2 === 0 ? 'yes' : 'no'}`,
     };
   },
-  calcGameQuestion: function () {
+  calcGameQuestion: function getQuestionsAboutCalc() {
     const operators = ['+', '-', '*'];
     const num1 = _.random(1, 20);
     const num2 = _.random(1, 20);
     const currentOperator = operators[_.random(0, operators.length - 1)];
 
+    function calc(num1, num2, operator) {
+      switch (operator) {
+        case '+':
+          return num1 + num2;
+        case '-':
+          return num1 - num2;
+        case '*':
+          return num1 * num2;
+      }
+    }
+
     return {
       question: `${num1} ${currentOperator} ${num2}`,
-      correctAnswer: eval(`${num1} ${currentOperator} ${num2}`),
+      correctAnswer: `${calc(num1, num2, currentOperator)}`,
     };
   },
 };
