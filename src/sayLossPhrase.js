6b272359c3d090155ccd2ac3f48a4868bebfe2cc
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-03-23 11:21:08 +0500

    refactor: move function sayLossPhrase.js to phrase.js
    there is a special library with phrases, so move the function there

--- src/sayLossPhrase.js
+++
@@ -1,5 +0,0 @@
-function sayLossPhrase(currect, yourAnswer, name) {
-  return `'${yourAnswer}' is wrong answer ;(. Correct answer was '${currect}'. \nLet's try again, ${name}!`;
-}
-
-export default sayLossPhrase;
