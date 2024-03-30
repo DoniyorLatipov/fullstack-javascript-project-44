c2f8ed29c4bcd2dd4311fb44c5828eba08df7727
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-03-30 12:01:30 +0500

    refactor primeGame.js:  move and refactor "doesItPrime" function
    * move the doesItPtime function above the questions logic so you
      don't have to create it every time
    * now it returns only true/false and the correct answers generates
      based on this result

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -1,30 +1,30 @@
 import _ from 'lodash';
 
+function doesItPrime(val) {
+  if (val === 1) {
+    return false;
+  }
+  if ([2, 3, 5, 7].includes(val)) {
+    return true;
+  }
+
+  // we start with 2 because all numbers are divisible by 1
+  for (let i = 2; i <= Math.sqrt(val); i += 1) {
+    if (val % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 function getPrimeQuestion() {
   // random number from 1 to 100 for prime game
   const value = _.random(1, 100);
 
-  function doesItPrime(val) {
-    if (val === 1) {
-      return 'no';
-    }
-    if ([2, 3, 5, 7].includes(val)) {
-      return 'yes';
-    }
-
-    // we start with 2 because all numbers are divisible by 1
-    for (let i = 2; i <= Math.sqrt(val); i += 1) {
-      if (val % i === 0) {
-        return 'no';
-      }
-    }
-
-    return 'yes';
-  }
-
   return {
     question: value,
-    correctAnswer: `${doesItPrime(value)}`,
+    correctAnswer: `${doesItPrime(value) ? 'yes' : 'no'}`,
   };
 }
 
