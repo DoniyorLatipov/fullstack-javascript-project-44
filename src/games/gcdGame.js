6667c040da49b7371ea70ff05ca75fc78886feaf
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-03-30 11:56:10 +0500

    refactor gcdGame.js: move "gcd" function above the question logic
    move the gcd function above the logic so you don't have to create
    it every time

--- src/games/gcdGame.js
+++ src/games/gcdGame.js
@@ -1,22 +1,22 @@
 import _ from 'lodash';
 
+function gcd(val1, val2) {
+  let number1 = val1 > val2 ? val1 : val2;
+  let number2 = val1 > val2 ? val2 : val1;
+
+  while (number2 !== 0) {
+    const temp = number1;
+    number1 = number2;
+    number2 = temp % number2;
+  }
+
+  return number1;
+}
+
 function getGcdQuestion() {
   // two randoms numbers from 1 to 100 for GCD game
   const num1 = _.random(1, 100);
   const num2 = _.random(1, 100);
-
-  function gcd(val1, val2) {
-    let number1 = val1 > val2 ? val1 : val2;
-    let number2 = val1 > val2 ? val2 : val1;
-
-    while (number2 !== 0) {
-      const temp = number1;
-      number1 = number2;
-      number2 = temp % number2;
-    }
-
-    return number1;
-  }
 
   return {
     question: `${num1} ${num2}`,
