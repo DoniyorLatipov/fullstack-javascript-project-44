19c2d06e97b9095f2b4f825d5b71a5339306c8fb
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-03-30 11:42:01 +0500

    refactor calcGame.js: move "calc" function above the question logic
    * move the Calc function above the logic so you don't have to create
      it every time
    * use "throw new error" in default case instead of "return null"

--- src/games/calcGame.js
+++ src/games/calcGame.js
@@ -1,24 +1,24 @@
 import _ from 'lodash';
 
+function calc(number1, number2, operator) {
+  switch (operator) {
+    case '+':
+      return number1 + number2;
+    case '-':
+      return number1 - number2;
+    case '*':
+      return number1 * number2;
+    default:
+      throw new Error(`Invalid operator - ${operator}`);
+  }
+}
+
 function getCalcQuestion() {
   const operators = ['+', '-', '*'];
   // random numbers from 1 to 20 for calculation game
   const num1 = _.random(1, 20);
   const num2 = _.random(1, 20);
   const currentOperator = operators[_.random(0, operators.length - 1)];
-
-  function calc(number1, number2, operator) {
-    switch (operator) {
-      case '+':
-        return number1 + number2;
-      case '-':
-        return number1 - number2;
-      case '*':
-        return number1 * number2;
-      default:
-        return null;
-    }
-  }
 
   return {
     question: `${num1} ${currentOperator} ${num2}`,
